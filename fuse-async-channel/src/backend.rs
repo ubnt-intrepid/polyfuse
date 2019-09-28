36645fd8f1f7656e190fc7feac106435c00c340b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-09-28 16:13:20 +0900

    switch to libfuse2

--- fuse-async-channel/src/backend.rs
+++ fuse-async-channel/src/backend.rs
@@ -1,55 +1,42 @@
-//! libfuse3 bindings.
+//! libfuse bindings.
 
 #![allow(nonstandard_style)]
 
 use libc::{c_char, c_int};
 use std::{
     ffi::{CString, OsStr}, //
-    fmt,
     io,
     os::unix::{
         ffi::OsStrExt,
         io::{AsRawFd, RawFd},
     },
     path::Path,
-    ptr::NonNull,
 };
 
 #[repr(C)]
-struct fuse_session {
-    _unused: [u8; 0],
+struct fuse_args {
+    argc: c_int,
+    argv: *const *const c_char,
+    allocated: c_int,
 }
 
 extern "C" {
-    fn fuse_session_destroy(se: *mut fuse_session);
-    fn fuse_session_fd(se: *mut fuse_session) -> c_int;
-    fn fuse_session_mount(se: *mut fuse_session, mountpoint: *const c_char) -> c_int;
-    fn fuse_session_unmount(se: *mut fuse_session);
+    fn fuse_mount_compat25(mountpoint: *const c_char, args: *mut fuse_args) -> c_int;
+    fn fuse_unmount_compat22(mountpoint: *const c_char);
+    fn fuse_opt_free_args(args: *mut fuse_args);
 }
 
-extern "C" {
-    fn fuse_session_new_empty(argc: c_int, argv: *const *const c_char) -> *mut fuse_session;
-}
-
+#[derive(Debug)]
 pub(crate) struct Connection {
-    se: NonNull<fuse_session>,
     raw_fd: RawFd,
+    mountpoint: CString,
 }
 
 impl Drop for Connection {
     fn drop(&mut self) {
         unsafe {
-            fuse_session_unmount(self.se.as_ptr());
-            fuse_session_destroy(self.se.as_ptr());
+            fuse_unmount_compat22(self.mountpoint.as_ptr());
         }
-    }
-}
-
-impl fmt::Debug for Connection {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("Channel")
-            .field("raw_fd", &self.raw_fd)
-            .finish()
     }
 }
 
@@ -62,28 +49,32 @@ impl Connection {
         let fsname = fsname.as_ref();
         let mountpoint = mountpoint.as_ref();
 
+        let c_mountpoint = CString::new(mountpoint.as_os_str().as_bytes())?;
+
         let mut args = vec![CString::new(fsname.as_bytes())?];
         for opt in mountopts {
             args.push(CString::new(opt.as_ref().as_bytes())?);
         }
         let c_args: Vec<*const c_char> = args.iter().map(|arg| arg.as_ptr()).collect();
 
-        let se =
-            NonNull::new(unsafe { fuse_session_new_empty(c_args.len() as c_int, c_args.as_ptr()) }) //
-                .ok_or_else(|| io::Error::last_os_error())?;
+        let mut f_args = fuse_args {
+            argc: c_args.len() as c_int,
+            argv: c_args.as_ptr(),
+            allocated: 0,
+        };
 
+        let raw_fd = unsafe { fuse_mount_compat25(c_mountpoint.as_ptr(), &mut f_args) };
         unsafe {
-            let c_mountpoint = CString::new(mountpoint.as_os_str().as_bytes())?;
-
-            let res = fuse_session_mount(se.as_ptr(), c_mountpoint.as_ptr());
-            if res == -1 {
-                return Err(io::Error::last_os_error());
-            }
+            fuse_opt_free_args(&mut f_args);
+        }
+        if raw_fd == -1 {
+            return Err(io::Error::last_os_error());
         }
 
-        let raw_fd = unsafe { fuse_session_fd(se.as_ptr()) };
-
-        Ok(Connection { se, raw_fd })
+        Ok(Connection {
+            raw_fd,
+            mountpoint: c_mountpoint,
+        })
     }
 }
 
