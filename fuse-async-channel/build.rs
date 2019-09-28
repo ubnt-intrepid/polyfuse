6f1ade1a4e6b50656995f49d7c4d8cb364684dc4
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-09-28 15:43:17 +0900

    add macOS target

--- fuse-async-channel/build.rs
+++ fuse-async-channel/build.rs
@@ -1,5 +1,10 @@
 const LIBFUSE_ATLEAST_VERSION: &str = "2.6.0";
+
+#[cfg(not(target_os = "macos"))]
 const LIBFUSE_PKG_NAME: &str = "fuse";
+
+#[cfg(target_os = "macos")]
+const LIBFUSE_PKG_NAME: &str = "osxfuse";
 
 fn main() {
     pkg_config::Config::new()
