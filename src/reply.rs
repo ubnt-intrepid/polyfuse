6d9fbdc4b6f48ce7af736c164f799fe2b1618221
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-10-06 21:55:01 +0900

    add DirEntry

--- src/reply.rs
+++ src/reply.rs
@@ -3,9 +3,11 @@
 use crate::abi::{
     AttrOut, //
     BmapOut,
+    DirEntry as DirEntryHeader,
     EntryOut,
     GetxattrOut,
     LkOut,
+    Nodeid,
     OpenOut,
     OutHeader,
     StatfsOut,
@@ -13,12 +15,122 @@ use crate::abi::{
     WriteOut,
 };
 use futures::io::{AsyncWrite, AsyncWriteExt};
+use memoffset::offset_of;
 use smallvec::SmallVec;
 use std::{
+    ffi::OsStr,
     fmt,
     io::{self, IoSlice},
     mem,
+    os::unix::ffi::OsStrExt,
+    ptr,
 };
+
+fn aligned(len: usize) -> usize {
+    (len + mem::size_of::<u64>() - 1) & !(mem::size_of::<u64>() - 1)
+}
+
+#[derive(Debug)]
+pub struct DirEntry {
+    dirent_buf: Vec<u8>,
+}
+
+impl DirEntry {
+    pub fn new(name: impl AsRef<OsStr>) -> Self {
+        let name = name.as_ref().as_bytes();
+        let entlen = mem::size_of::<DirEntryHeader>() + name.len();
+        let entsize = aligned(entlen);
+        let padlen = entsize - entlen;
+        let name_offset = offset_of!(DirEntryHeader, name);
+
+        let mut dirent_buf = Vec::<u8>::with_capacity(entsize);
+        unsafe {
+            let p = dirent_buf.as_mut_ptr();
+
+            let pheader = p as *mut DirEntryHeader;
+            (*pheader).ino = Nodeid::from_raw(0);
+            (*pheader).off = 0;
+            (*pheader).namelen = name.len() as u32;
+            (*pheader).typ = 0;
+
+            let p = p.offset(name_offset as isize);
+            ptr::copy_nonoverlapping(name.as_ptr(), p, name.len());
+
+            let p = p.offset(name.len() as isize);
+            ptr::write_bytes(p, 0u8, padlen);
+
+            dirent_buf.set_len(entsize);
+        }
+
+        Self { dirent_buf }
+    }
+
+    unsafe fn header(&self) -> &DirEntryHeader {
+        debug_assert!(self.dirent_buf.len() > mem::size_of::<DirEntryHeader>());
+        &*(self.dirent_buf.as_ptr() as *mut DirEntryHeader)
+    }
+
+    unsafe fn header_mut(&mut self) -> &mut DirEntryHeader {
+        debug_assert!(self.dirent_buf.len() > mem::size_of::<DirEntryHeader>());
+        &mut *(self.dirent_buf.as_mut_ptr() as *mut DirEntryHeader)
+    }
+
+    pub fn nodeid(&self) -> Nodeid {
+        unsafe { self.header().ino }
+    }
+
+    pub fn nodeid_mut(&mut self) -> &mut Nodeid {
+        unsafe { &mut self.header_mut().ino }
+    }
+
+    pub fn offset(&self) -> u64 {
+        unsafe { self.header().off }
+    }
+
+    pub fn offset_mut(&mut self) -> &mut u64 {
+        unsafe { &mut self.header_mut().off }
+    }
+
+    pub fn type_(&self) -> u32 {
+        unsafe { self.header().typ }
+    }
+
+    pub fn type_mut(&mut self) -> &mut u32 {
+        unsafe { &mut self.header_mut().typ }
+    }
+
+    pub fn name(&self) -> &OsStr {
+        let name_offset = offset_of!(DirEntryHeader, name);
+        let namelen = unsafe { self.header().namelen as usize };
+        OsStr::from_bytes(&self.dirent_buf[name_offset..name_offset + namelen])
+    }
+
+    pub fn set_name(&mut self, name: impl AsRef<OsStr>) {
+        let name = name.as_ref().as_bytes();
+        let entlen = mem::size_of::<DirEntryHeader>() + name.len();
+        let entsize = aligned(entlen);
+        let padlen = entsize - entlen;
+        let name_offset = offset_of!(DirEntryHeader, name);
+
+        self.dirent_buf.resize(entsize, 0);
+        unsafe {
+            let p = self.dirent_buf.as_mut_ptr();
+            (*(p as *mut DirEntryHeader)).namelen = name.len() as u32;
+
+            let p = p.offset(name_offset as isize);
+            ptr::copy_nonoverlapping(name.as_ptr(), p, name.len());
+
+            let p = p.offset(name.len() as isize);
+            ptr::write_bytes(p, 0u8, padlen);
+        }
+    }
+}
+
+impl AsRef<[u8]> for DirEntry {
+    fn as_ref(&self) -> &[u8] {
+        self.dirent_buf.as_ref()
+    }
+}
 
 pub struct ReplyRaw<'a> {
     io: &'a mut (dyn AsyncWrite + Unpin),
@@ -311,5 +423,78 @@ impl<'a> ReplyBmap<'a> {
 
     pub async fn err(self, errno: i32) -> io::Result<()> {
         self.raw.reply_err(errno).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_direntry() {
+        let dirent = DirEntry::new("hello");
+        assert_eq!(dirent.nodeid(), Nodeid::from_raw(0));
+        assert_eq!(dirent.offset(), 0u64);
+        assert_eq!(dirent.type_(), 0u32);
+        assert_eq!(dirent.name(), "hello");
+
+        assert_eq!(dirent.as_ref().len(), 32usize);
+        assert_eq!(
+            dirent.as_ref(),
+            &*vec![
+                0u8, 0, 0, 0, 0, 0, 0, 0, // ino
+                0, 0, 0, 0, 0, 0, 0, 0, // off
+                5, 0, 0, 0, // namlen
+                0, 0, 0, 0, // typ
+                104, 101, 108, 108, 111, // name
+                0, 0, 0 // padding
+            ]
+        );
+    }
+
+    #[test]
+    fn test_direntry_set_long_name() {
+        let mut dirent = DirEntry::new("hello");
+        dirent.set_name("good evening");
+        assert_eq!(dirent.nodeid(), Nodeid::from_raw(0));
+        assert_eq!(dirent.offset(), 0u64);
+        assert_eq!(dirent.type_(), 0u32);
+        assert_eq!(dirent.name(), "good evening");
+
+        assert_eq!(dirent.as_ref().len(), 40usize);
+        assert_eq!(
+            dirent.as_ref(),
+            &*vec![
+                0u8, 0, 0, 0, 0, 0, 0, 0, // ino
+                0, 0, 0, 0, 0, 0, 0, 0, // off
+                12, 0, 0, 0, // namelen
+                0, 0, 0, 0, // typ
+                103, 111, 111, 100, 32, 101, 118, 101, 110, 105, 110, 103, // name
+                0, 0, 0, 0 // padding
+            ]
+        );
+    }
+
+    #[test]
+    fn test_direntry_set_short_name() {
+        let mut dirent = DirEntry::new("good morning");
+        dirent.set_name("bye");
+        assert_eq!(dirent.nodeid(), Nodeid::from_raw(0));
+        assert_eq!(dirent.offset(), 0u64);
+        assert_eq!(dirent.type_(), 0u32);
+        assert_eq!(dirent.name(), "bye");
+
+        assert_eq!(dirent.as_ref().len(), 32usize);
+        assert_eq!(
+            dirent.as_ref(),
+            &*vec![
+                0u8, 0, 0, 0, 0, 0, 0, 0, // ino
+                0, 0, 0, 0, 0, 0, 0, 0, // off
+                3, 0, 0, 0, // namelen
+                0, 0, 0, 0, // off
+                98, 121, 101, // name
+                0, 0, 0, 0, 0 // padding
+            ]
+        );
     }
 }
