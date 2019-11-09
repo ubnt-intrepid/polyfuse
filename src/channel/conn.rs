ca07f6adfb3edb77f7d6ecfc2fafcc985954bb77
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-11-10 01:43:22 +0900

    refactor channel/conn.rs

--- src/channel/conn.rs
+++ src/channel/conn.rs
@@ -1,14 +1,14 @@
-// FIXME: re-enable lint rules
 #![allow(
     clippy::cast_possible_wrap,
     clippy::cast_possible_truncation,
     clippy::cast_sign_loss
 )]
 
-use libc::{c_int, c_void, iovec};
+use libc::{c_int, c_void, iovec, ssize_t};
 use mio::{unix::EventedFd, Evented, PollOpt, Ready, Token};
 use polyfuse_sys::kernel::FUSE_DEV_IOC_CLONE;
 use std::{
+    cmp,
     ffi::{CStr, OsStr, OsString},
     io::{self, IoSlice, IoSliceMut, Read, Write},
     mem::{self, MaybeUninit},
@@ -56,39 +56,13 @@ impl Drop for Connection {
 impl Connection {
     /// Establish a new connection with the FUSE kernel driver.
     pub fn open(mountpoint: &Path, mountopts: &MountOptions) -> io::Result<Self> {
-        let (reader, writer) = UnixDatagram::pair()?;
+        let (_pid, mut reader) = exec_fusermount(mountpoint, mountopts)?;
 
-        let pid = unsafe { libc::fork() };
-        if pid == -1 {
-            return Err(io::Error::last_os_error());
-        }
-
-        if pid == 0 {
-            drop(reader);
-            let writer = writer.into_raw_fd();
-            unsafe { libc::fcntl(writer, libc::F_SETFD, 0) };
-
-            let mut fusermount = Command::new(FUSERMOUNT_PROG);
-            fusermount.env(FUSE_COMMFD_ENV, writer.to_string());
-            if let Some(opts) = mountopts.options() {
-                fusermount.arg("-o").arg(opts);
-            }
-            fusermount.arg("--").arg(mountpoint);
-            return Err(fusermount.exec());
-        }
-
-        drop(writer);
-
-        let fd = receive_fd(reader.as_raw_fd())?;
+        let fd = receive_fd(&mut reader)?;
         set_nonblocking(fd)?;
 
-        // TODO: treat auto_unmount option.
-        // if !auto_unmount {
-        //     drop(rd);
-        //     unsafe {
-        //         libc::waitpid(pid, ptr::null_mut(), 0); // bury zombie.
-        //     }
-        // }
+        // Unmounting is executed when `reader` is dropped and the connection
+        // with `fusermount` is closed.
         let _ = reader.into_raw_fd();
 
         Ok(Self {
@@ -104,22 +78,14 @@ impl Connection {
             if ioc_clone {
                 let devname = CStr::from_bytes_with_nul_unchecked(b"/dev/fuse\0");
 
-                clonefd = libc::open(devname.as_ptr(), libc::O_RDWR | libc::O_CLOEXEC);
-                if clonefd == -1 {
-                    return Err(io::Error::last_os_error());
-                }
+                clonefd = cvt(libc::open(devname.as_ptr(), libc::O_RDWR | libc::O_CLOEXEC))?;
 
-                let res = libc::ioctl(clonefd, FUSE_DEV_IOC_CLONE.into(), &self.fd);
-                if res == -1 {
-                    let err = io::Error::last_os_error();
+                if let Err(err) = cvt(libc::ioctl(clonefd, FUSE_DEV_IOC_CLONE.into(), &self.fd)) {
                     libc::close(clonefd);
                     return Err(err);
                 }
             } else {
-                clonefd = libc::dup(self.fd);
-                if clonefd == -1 {
-                    return Err(io::Error::last_os_error());
-                }
+                clonefd = cvt(libc::dup(self.fd))?;
             }
         }
 
@@ -140,7 +106,31 @@ impl Connection {
     }
 }
 
-fn receive_fd(fd_in: RawFd) -> io::Result<RawFd> {
+fn exec_fusermount(
+    mountpoint: &Path,
+    mountopts: &MountOptions,
+) -> io::Result<(c_int, UnixDatagram)> {
+    let (reader, writer) = UnixDatagram::pair()?;
+
+    let pid = cvt(unsafe { libc::fork() })?;
+    if pid == 0 {
+        drop(reader);
+        let writer = writer.into_raw_fd();
+        unsafe { libc::fcntl(writer, libc::F_SETFD, 0) };
+
+        let mut fusermount = Command::new(FUSERMOUNT_PROG);
+        fusermount.env(FUSE_COMMFD_ENV, writer.to_string());
+        if let Some(opts) = mountopts.options() {
+            fusermount.arg("-o").arg(opts);
+        }
+        fusermount.arg("--").arg(mountpoint);
+        return Err(fusermount.exec());
+    }
+
+    Ok((pid, reader))
+}
+
+fn receive_fd(reader: &mut UnixDatagram) -> io::Result<RawFd> {
     let mut buf = [0u8; 1];
     let mut iov = libc::iovec {
         iov_base: buf.as_mut_ptr() as *mut c_void,
@@ -164,10 +154,7 @@ fn receive_fd(fd_in: RawFd) -> io::Result<RawFd> {
         msg_flags: 0,
     };
 
-    let ret = unsafe { libc::recvmsg(fd_in, &mut msg, 0) };
-    if ret == -1 {
-        return Err(io::Error::last_os_error());
-    }
+    cvt(unsafe { libc::recvmsg(reader.as_raw_fd(), &mut msg, 0) })?;
 
     if msg.msg_controllen < mem::size_of_val(&cmsg) {
         return Err(io::Error::new(
@@ -195,68 +182,53 @@ impl AsRawFd for Connection {
 
 impl Read for Connection {
     fn read(&mut self, dst: &mut [u8]) -> io::Result<usize> {
-        let res = unsafe {
+        let res = cvt(unsafe {
             libc::read(
                 self.fd, //
                 dst.as_mut_ptr() as *mut c_void,
                 dst.len(),
             )
-        };
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        })?;
         Ok(res as usize)
     }
 
     fn read_vectored(&mut self, dst: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
-        let res = unsafe {
+        let res = cvt(unsafe {
             libc::readv(
                 self.fd, //
                 dst.as_mut_ptr() as *mut iovec,
-                dst.len() as c_int,
+                cmp::min(dst.len(), c_int::max_value() as usize) as c_int,
             )
-        };
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        })?;
         Ok(res as usize)
     }
 }
 
 impl Write for Connection {
     fn write(&mut self, src: &[u8]) -> io::Result<usize> {
-        let res = unsafe {
+        let res = cvt(unsafe {
             libc::write(
                 self.fd, //
                 src.as_ptr() as *const c_void,
                 src.len(),
             )
-        };
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        })?;
         Ok(res as usize)
     }
 
     fn write_vectored(&mut self, src: &[IoSlice]) -> io::Result<usize> {
-        let res = unsafe {
+        let res = cvt(unsafe {
             libc::writev(
                 self.fd, //
                 src.as_ptr() as *const iovec,
-                src.len() as c_int,
+                cmp::min(src.len(), c_int::max_value() as usize) as c_int,
             )
-        };
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        })?;
         Ok(res as usize)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        let res = unsafe { libc::fsync(self.fd) };
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        cvt(unsafe { libc::fsync(self.fd) })?;
         Ok(())
     }
 }
@@ -287,16 +259,33 @@ impl Evented for Connection {
     }
 }
 
+// copied from libstd/sys/unix/mod.rs
+fn cvt<T: IsMinusOne>(t: T) -> io::Result<T> {
+    if t.is_minus_one() {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(t)
+    }
+}
+
+trait IsMinusOne {
+    fn is_minus_one(&self) -> bool;
+}
+
+impl IsMinusOne for c_int {
+    fn is_minus_one(&self) -> bool {
+        *self == -1
+    }
+}
+
+impl IsMinusOne for ssize_t {
+    fn is_minus_one(&self) -> bool {
+        *self == -1
+    }
+}
+
 fn set_nonblocking(fd: RawFd) -> io::Result<()> {
-    let flags = unsafe { libc::fcntl(fd, libc::F_GETFL, 0) };
-    if flags < 0 {
-        return Err(io::Error::last_os_error());
-    }
-
-    let res = unsafe { libc::fcntl(fd, libc::F_SETFL, flags | libc::O_NONBLOCK) };
-    if res < 0 {
-        return Err(io::Error::last_os_error());
-    }
-
+    let flags = cvt(unsafe { libc::fcntl(fd, libc::F_GETFL, 0) })?;
+    cvt(unsafe { libc::fcntl(fd, libc::F_SETFL, flags | libc::O_NONBLOCK) })?;
     Ok(())
 }
