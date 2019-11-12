4798aa816f1795ce149d1fb49531a17ac8325365
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-11-13 00:17:22 +0900

    remove fuzzing test

--- fuzz/fuzz_targets/request_parser.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use polyfuse::abi::parse::Parser;
-
-fuzz_target!(|data: &[u8]| {
-    let mut parser = Parser::new(data);
-    let _ = parser.parse();
-});
