b22ed3cb4927bd75a0ac9331bb900d59d4d40683
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-11-19 00:23:14 +0900

    fix xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,21 +1,30 @@
-use clap::{App, AppSettings, ArgMatches, SubCommand};
-use std::path::{Path, PathBuf};
+use std::{
+    ffi::OsString,
+    path::{Path, PathBuf},
+};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Arg {
+    /// Install Git hooks
+    InstallHooks,
+
+    /// Run a script.
+    Script {
+        /// The script name
+        #[structopt(name = "command", parse(from_os_str))]
+        command: OsString,
+
+        /// The arguments passed to script
+        #[structopt(name = "args", parse(from_os_str))]
+        args: Vec<OsString>,
+    },
+}
 
 fn main() -> anyhow::Result<()> {
-    let app = App::new("automation scripts")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(SubCommand::with_name("install-hooks"))
-        .subcommand(SubCommand::with_name("run").args_from_usage(
-            "<command> 'The name of script command'\n\
-             [args]..  'Argument of script'",
-        ));
-
-    let matches = app.get_matches();
-    match matches.subcommand() {
-        ("install-hooks", _) => do_install_hooks(),
-        ("run", Some(arg)) => do_run(arg),
-        (_, _) => unreachable!(),
+    match Arg::from_args() {
+        Arg::InstallHooks => do_install_hooks(),
+        Arg::Script { command, args } => do_run(command, args),
     }
 }
 
@@ -50,16 +59,14 @@ fn install_hook(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<
     Ok(())
 }
 
-fn do_run(arg: &ArgMatches<'_>) -> anyhow::Result<()> {
-    let command = arg.value_of_os("command").unwrap();
-    let args = arg.values_of_os("args");
-
+fn do_run(command: OsString, args: Vec<OsString>) -> anyhow::Result<()> {
     let bin_dir = project_root()?.join("bin").canonicalize()?;
     anyhow::ensure!(bin_dir.is_dir(), "bin/ is not a directory");
 
     use std::process::{Command, Stdio};
 
     let mut script = Command::new(command);
+    script.args(args);
     script.stdin(Stdio::null());
     script.stdout(Stdio::inherit());
     script.stderr(Stdio::inherit());
@@ -70,9 +77,6 @@ fn do_run(arg: &ArgMatches<'_>) -> anyhow::Result<()> {
             .collect();
         let new_path = std::env::join_paths(paths)?;
         script.env("PATH", new_path);
-    }
-    if let Some(args) = args {
-        script.args(args);
     }
 
     let status = script.status()?;
