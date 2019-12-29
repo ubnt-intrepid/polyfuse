0cd8d5f9ab2e4bbaa90591c00a62a68fdaea4c1c
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-12-29 15:10:12 +0900

    update examples/hello.rs

--- examples/examples/hello.rs
+++ examples/examples/hello.rs
@@ -4,12 +4,17 @@
 use polyfuse_examples::prelude::*;
 
 use polyfuse::{
+    op,
     reply::{ReplyAttr, ReplyEntry},
     DirEntry, FileAttr,
 };
 use std::io;
 
 const TTL: Duration = Duration::from_secs(60 * 60 * 24 * 365);
+const ROOT_INO: u64 = 1;
+const HELLO_INO: u64 = 2;
+const HELLO_FILENAME: &str = "hello.txt";
+const HELLO_CONTENT: &[u8] = b"Hello, world!\n";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -24,85 +29,115 @@ async fn main() -> anyhow::Result<()> {
 }
 
 struct Hello {
-    filename: String,
-    content: String,
+    root_attr: FileAttr,
+    hello_attr: FileAttr,
     dir_entries: Vec<DirEntry>,
 }
 
 impl Hello {
     fn new() -> Self {
-        let filename = "hello.txt".to_string();
-        let content = "Hello, World!\n".to_string();
+        let root_attr = root_attr();
+        let mut hello_attr = hello_attr();
+        hello_attr.set_size(HELLO_CONTENT.len() as u64);
+
         let dir_entries = {
             let mut entries = Vec::with_capacity(3);
             entries.push(DirEntry::dir(".", 1, 1));
             entries.push(DirEntry::dir("..", 1, 2));
-            entries.push(DirEntry::file(&filename, 2, 3));
+            entries.push(DirEntry::file(HELLO_FILENAME, 2, 3));
             entries
         };
 
         Self {
-            filename,
-            content,
+            root_attr,
+            hello_attr,
             dir_entries,
         }
     }
 
-    fn root_attr(&self) -> FileAttr {
-        let mut attr = FileAttr::default();
-        attr.set_mode(libc::S_IFDIR as u32 | 0o555);
-        attr.set_ino(1);
-        attr.set_nlink(2);
-        attr.set_uid(unsafe { libc::getuid() });
-        attr.set_gid(unsafe { libc::getgid() });
-        attr
-    }
-
-    fn hello_attr(&self) -> FileAttr {
-        let mut attr = FileAttr::default();
-        attr.set_mode(libc::S_IFREG as u32 | 0o444);
-        attr.set_ino(2);
-        attr.set_nlink(1);
-        attr.set_size(self.content.len() as u64);
-        attr.set_uid(unsafe { libc::getuid() });
-        attr.set_gid(unsafe { libc::getgid() });
-        attr
-    }
-
-    fn lookup(&self, parent: u64, name: &OsStr) -> Option<FileAttr> {
-        match parent {
-            1 if name.as_bytes() == self.filename.as_bytes() => Some(self.hello_attr()),
-            _ => None,
-        }
-    }
-
-    fn getattr(&self, ino: u64) -> Option<FileAttr> {
-        match ino {
-            1 => Some(self.root_attr()),
-            2 => Some(self.hello_attr()),
-            _ => None,
-        }
-    }
-
-    async fn read(&self, ino: u64, offset: usize, size: usize) -> Result<&[u8], libc::c_int> {
-        match ino {
-            1 => Err(libc::EISDIR),
-            2 => {
-                if offset >= self.content.len() {
-                    return Ok(&[] as &[u8]);
-                }
-                let data = &self.content.as_bytes()[offset..];
-                let data = &data[..std::cmp::min(data.len(), size)];
-                Ok(data)
+    async fn do_lookup<T: ?Sized>(
+        &self,
+        cx: &mut Context<'_, T>,
+        op: op::Lookup<'_>,
+    ) -> io::Result<()>
+    where
+        T: Writer + Unpin,
+    {
+        match op.parent() {
+            ROOT_INO if op.name().as_bytes() == HELLO_FILENAME.as_bytes() => {
+                op.reply(cx, {
+                    ReplyEntry::new(self.hello_attr) //
+                        .ttl_attr(TTL)
+                        .ttl_entry(TTL)
+                })
+                .await?;
             }
-            _ => Err(libc::ENOENT),
+            _ => cx.reply_err(libc::ENOENT).await?,
         }
+
+        Ok(())
     }
 
-    fn readdir(&self, ino: u64, offset: u64, size: usize) -> Result<Vec<&[u8]>, libc::c_int> {
-        if ino != 1 {
-            return Err(libc::ENOENT);
+    async fn do_getattr<T: ?Sized>(
+        &self,
+        cx: &mut Context<'_, T>,
+        op: op::Getattr<'_>,
+    ) -> io::Result<()>
+    where
+        T: Writer + Unpin,
+    {
+        let attr = match op.ino() {
+            ROOT_INO => self.root_attr,
+            HELLO_INO => self.hello_attr,
+            _ => return cx.reply_err(libc::ENOENT).await,
+        };
+
+        op.reply(cx, {
+            ReplyAttr::new(attr) //
+                .ttl_attr(TTL)
+        })
+        .await?;
+
+        Ok(())
+    }
+
+    async fn do_read<T: ?Sized>(&self, cx: &mut Context<'_, T>, op: op::Read<'_>) -> io::Result<()>
+    where
+        T: Writer + Unpin,
+    {
+        match op.ino() {
+            ROOT_INO => return cx.reply_err(libc::EISDIR).await,
+            HELLO_INO => (),
+            _ => return cx.reply_err(libc::ENOENT).await,
         }
+
+        let offset = op.offset() as usize;
+        if offset >= HELLO_CONTENT.len() {
+            return op.reply(cx, &[]).await;
+        }
+
+        let size = op.size() as usize;
+        let data = &HELLO_CONTENT[offset..];
+        let data = &data[..std::cmp::min(data.len(), size)];
+        op.reply(cx, data).await?;
+
+        Ok(())
+    }
+
+    async fn do_readdir<T: ?Sized>(
+        &self,
+        cx: &mut Context<'_, T>,
+        op: op::Readdir<'_>,
+    ) -> io::Result<()>
+    where
+        T: Writer + Unpin,
+    {
+        if op.ino() != ROOT_INO {
+            return cx.reply_err(libc::ENOTDIR).await;
+        }
+
+        let offset = op.offset() as usize;
+        let size = op.size() as usize;
 
         let mut entries = Vec::with_capacity(3);
         let mut total_len = 0usize;
@@ -115,7 +150,9 @@ impl Hello {
             total_len += entry.len();
         }
 
-        Ok(entries)
+        op.reply_vectored(cx, &*entries).await?;
+
+        Ok(())
     }
 }
 
@@ -130,48 +167,31 @@ impl Filesystem for Hello {
         T: Reader + Writer + Unpin + Send,
     {
         match op {
-            Operation::Lookup(op) => match self.lookup(op.parent(), op.name()) {
-                Some(attr) => {
-                    op.reply(cx, {
-                        ReplyEntry::new(attr) //
-                            .ttl_attr(TTL)
-                            .ttl_entry(TTL)
-                    })
-                    .await?;
-                }
-                None => cx.reply_err(libc::ENOENT).await?,
-            },
-
-            Operation::Getattr(op) => match self.getattr(op.ino()) {
-                Some(attr) => {
-                    op.reply(cx, {
-                        ReplyAttr::new(attr) //
-                            .ttl_attr(TTL)
-                    })
-                    .await?;
-                }
-                None => cx.reply_err(libc::ENOENT).await?,
-            },
-
-            Operation::Read(op) => {
-                match self
-                    .read(op.ino(), op.offset() as usize, op.size() as usize)
-                    .await
-                {
-                    Ok(data) => op.reply(cx, data).await?,
-                    Err(errno) => cx.reply_err(errno).await?,
-                }
-            }
-
-            Operation::Readdir(op) => match self.readdir(op.ino(), op.offset(), op.size() as usize)
-            {
-                Ok(entries) => op.reply_vectored(cx, &*entries).await?,
-                Err(errno) => cx.reply_err(errno).await?,
-            },
-
-            _ => (),
+            Operation::Lookup(op) => self.do_lookup(cx, op).await,
+            Operation::Getattr(op) => self.do_getattr(cx, op).await,
+            Operation::Read(op) => self.do_read(cx, op).await,
+            Operation::Readdir(op) => self.do_readdir(cx, op).await,
+            _ => Ok(()),
         }
-
-        Ok(())
     }
+}
+
+fn root_attr() -> FileAttr {
+    let mut attr = FileAttr::default();
+    attr.set_mode(libc::S_IFDIR as u32 | 0o555);
+    attr.set_ino(ROOT_INO);
+    attr.set_nlink(2);
+    attr.set_uid(unsafe { libc::getuid() });
+    attr.set_gid(unsafe { libc::getgid() });
+    attr
+}
+
+fn hello_attr() -> FileAttr {
+    let mut attr = FileAttr::default();
+    attr.set_mode(libc::S_IFREG as u32 | 0o444);
+    attr.set_ino(HELLO_INO);
+    attr.set_nlink(1);
+    attr.set_uid(unsafe { libc::getuid() });
+    attr.set_gid(unsafe { libc::getgid() });
+    attr
 }
