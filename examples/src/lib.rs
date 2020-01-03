440975c43e874c71fd681d642539457b2c7f8dd6
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-01-04 01:22:27 +0900

    apply ScatteredBytes to examples/passthrough.rs

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -9,7 +9,7 @@ pub mod prelude {
     pub use futures::future::{Future, FutureExt};
     pub use polyfuse::{
         async_trait,
-        io::{Reader, Writer},
+        io::{Reader, ScatteredBytes, Writer},
         op::Operation,
         Context, Filesystem,
     };
@@ -28,4 +28,27 @@ pub fn get_mountpoint() -> anyhow::Result<std::path::PathBuf> {
         .nth(1)
         .map(Into::into)
         .ok_or_else(|| anyhow::anyhow!("missing mountpoint"))
+}
+
+// FIXME: use either crate.
+pub enum Either<L, R> {
+    Left(L),
+    Right(R),
+}
+
+impl<L, R> polyfuse::io::ScatteredBytes for Either<L, R>
+where
+    L: polyfuse::io::ScatteredBytes,
+    R: polyfuse::io::ScatteredBytes,
+{
+    #[inline]
+    fn collect<'a, C: ?Sized>(&'a self, collector: &mut C)
+    where
+        C: polyfuse::io::Collector<'a>,
+    {
+        match self {
+            Either::Left(l) => l.collect(collector),
+            Either::Right(r) => r.collect(collector),
+        }
+    }
 }
