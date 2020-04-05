0773a0a9f9c623bf22383939f5eb83d31d0f6f8c
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-04-06 01:27:46 +0900

    use pico-args instead of structopt

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,65 +1,76 @@
-use std::{
-    ffi::OsString,
-    path::{Path, PathBuf},
-};
-use structopt::StructOpt;
-
-#[derive(Debug, StructOpt)]
-enum Arg {
-    /// Install Git hooks
-    InstallHooks,
-
-    /// Run a script.
-    Script {
-        /// The script name
-        #[structopt(name = "command", parse(from_os_str))]
-        command: OsString,
-
-        /// The arguments passed to script
-        #[structopt(name = "args", parse(from_os_str))]
-        args: Vec<OsString>,
-    },
-}
+use pico_args::Arguments;
+use std::{ffi::OsString, path::PathBuf};
 
 fn main() -> anyhow::Result<()> {
-    match Arg::from_args() {
-        Arg::InstallHooks => do_install_hooks(),
-        Arg::Script { command, args } => do_run(command, args),
+    let show_help = || {
+        eprintln!(
+            "\
+cargo-xtask
+Free-form automation tool
+
+Usage:
+    cargo xtask <SUBCOMMAND>
+
+Subcommands:
+    script  Run external script
+
+Flags:
+    -h, --help  Show this message
+"
+        );
+    };
+
+    let mut args = Arguments::from_env();
+
+    if args.contains(["-h", "--help"]) {
+        show_help();
+        return Ok(());
+    }
+
+    let subcommand = args.subcommand()?.unwrap_or_default();
+    match &*subcommand {
+        "script" => {
+            let show_help = || {
+                eprintln!(
+                    "\
+cargo-xtask script
+Run external script
+
+Usage:
+    cargo xtask script <COMMAND> <ARGS>...
+
+Flags:
+    -h, --help  Show this message
+    <COMMAND>   Script name
+    <ARGS>...   Arguments passed to the script
+"
+                );
+            };
+
+            if args.contains(["-h", "--help"]) {
+                show_help();
+                return Ok(());
+            }
+
+            let command = args
+                .free_from_os_str(|arg| Ok::<_, std::convert::Infallible>(arg.to_owned()))?
+                .ok_or_else(|| {
+                    show_help();
+                    anyhow::anyhow!("missing script name")
+                })?;
+
+            let args = args.free_os()?;
+
+            do_script(command, args)
+        }
+        _ => {
+            show_help();
+            anyhow::bail!("invalid CLI arguments");
+        }
     }
 }
 
-fn do_install_hooks() -> anyhow::Result<()> {
-    let src_dir = project_root()?.join(".cargo/hooks").canonicalize()?;
-    anyhow::ensure!(src_dir.is_dir(), "Cargo hooks directory");
-
-    let dst_dir = project_root()?.join(".git/hooks").canonicalize()?;
-    anyhow::ensure!(dst_dir.is_dir(), "Git hooks directory");
-
-    install_hook(src_dir.join("pre-commit"), dst_dir.join("pre-commit"))?;
-    install_hook(src_dir.join("pre-push"), dst_dir.join("pre-push"))?;
-
-    Ok(())
-}
-
-fn install_hook(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
-    let src = src.as_ref();
-    let dst = dst.as_ref();
-
-    if src.is_file() {
-        println!("install {:?} to {:?}", src, dst);
-
-        std::fs::create_dir_all(
-            dst.parent()
-                .ok_or_else(|| anyhow::anyhow!("missing dst parent"))?,
-        )?;
-        std::fs::remove_file(dst)?;
-        std::os::unix::fs::symlink(src, dst)?;
-    }
-
-    Ok(())
-}
-
-fn do_run(command: OsString, args: Vec<OsString>) -> anyhow::Result<()> {
+fn do_script(command: OsString, args: Vec<OsString>) -> anyhow::Result<()> {
     let bin_dir = project_root()?.join("bin").canonicalize()?;
     anyhow::ensure!(bin_dir.is_dir(), "bin/ is not a directory");
 
