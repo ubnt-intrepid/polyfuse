c40594319b9b99c297f5898510f74cd7f84b8b5d
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-11-17 19:28:03 +0900

    rewrite example sketch

--- examples/sketch/src/main.rs
+++ examples/sketch/src/main.rs
@@ -1,39 +1,63 @@
-use polyfuse::op;
+use polyfuse::{op, reply::ReplyAttr as _, types::FileAttr};
 use polyfuse_daemon::Operation;
 
-fn main() -> anyhow::Result<()> {
-    async_std::task::block_on(async {
-        let mut daemon = polyfuse_daemon::mount("/path/to/mountpoint", &[]).await?;
+use anyhow::Context as _;
+use async_std::task;
+use std::path::PathBuf;
 
-        let fs = Filesystem {};
+fn main() -> anyhow::Result<()> {
+    let mut args = pico_args::Arguments::from_env();
+
+    let mountpoint: PathBuf = args
+        .free_from_str()?
+        .context("missing mountpoint specified")?;
+    anyhow::ensure!(mountpoint.is_file(), "mountpoint must be a regular file");
+
+    task::block_on(async {
+        // Start the FUSE daemon mounted on the specified path.
+        let mut daemon = polyfuse_daemon::mount(mountpoint, &[]).await?;
 
         loop {
+            // Receive an incoming FUSE request from the kernel.
             let mut req = match daemon.next_request().await {
                 Some(req) => req,
                 None => break,
             };
 
-            req.process(|op| fs.process(op)).await?;
+            // Process the request.
+            req.process(|op| async {
+                match op {
+                    // Dispatch your callbacks to the supported operations...
+                    Operation::Getattr(op) => getattr(op).await,
+
+                    // Or annotate that the operation is not supported.
+                    op => op.unimplemented().await,
+                }
+            })
+            .await?;
         }
 
         Ok(())
     })
 }
 
-struct Filesystem {}
-
-impl Filesystem {
-    async fn process(&self, op: Operation<'_>) -> polyfuse_daemon::op::Result {
-        match op {
-            Operation::Getattr(op) => self.getattr(op).await,
-            op => op.unimplemented().await,
-        }
+async fn getattr<Op>(op: Op) -> Result<Op::Ok, Op::Error>
+where
+    Op: op::Getattr,
+{
+    if op.ino() != 1 {
+        return Err(polyfuse::error::code(libc::ENOENT));
     }
 
-    async fn getattr<Op>(&self, _op: Op) -> Result<Op::Ok, Op::Error>
-    where
-        Op: op::Getattr,
-    {
-        todo!()
-    }
+    op.reply().attr(
+        &FileAttr {
+            ino: 1,
+            mode: libc::S_IFREG as u32 | 0o444,
+            nlink: 1,
+            uid: unsafe { libc::getuid() },
+            gid: unsafe { libc::getgid() },
+            ..Default::default()
+        },
+        None,
+    )
 }
