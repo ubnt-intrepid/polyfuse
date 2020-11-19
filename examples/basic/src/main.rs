a42f362adc3bf0ea3416a11f52cd85ac67816c38
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-11-19 13:53:06 +0900

    misc

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -2,10 +2,10 @@ use polyfuse::{op, reply::ReplyAttr as _, types::FileAttr};
 use polyfuse_daemon::{Daemon, Operation};
 
 use anyhow::Context as _;
-use async_std::task;
 use std::path::PathBuf;
 
-fn main() -> anyhow::Result<()> {
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let mut args = pico_args::Arguments::from_env();
@@ -15,29 +15,27 @@ fn main() -> anyhow::Result<()> {
         .context("missing mountpoint specified")?;
     anyhow::ensure!(mountpoint.is_file(), "mountpoint must be a regular file");
 
-    task::block_on(async {
-        // Start the FUSE daemon mounted on the specified path.
-        let mut daemon = Daemon::mount(mountpoint, &[]).await?;
+    // Start the FUSE daemon mounted on the specified path.
+    let mut daemon = Daemon::mount(mountpoint, &[]).await?;
 
-        // Receive an incoming FUSE request from the kernel.
-        while let Some(req) = daemon.next_request().await? {
-            // Process the request.
-            req.process(|op| async {
-                tracing::trace!("in process()");
+    // Receive an incoming FUSE request from the kernel.
+    while let Some(req) = daemon.next_request().await? {
+        // Process the request.
+        req.process(|op| async {
+            tracing::trace!("in process()");
 
-                match op {
-                    // Dispatch your callbacks to the supported operations...
-                    Operation::Getattr(op) => getattr(op).await,
+            match op {
+                // Dispatch your callbacks to the supported operations...
+                Operation::Getattr(op) => getattr(op).await,
 
-                    // Or annotate that the operation is not supported.
-                    op => op.unimplemented().await,
-                }
-            })
-            .await?;
-        }
+                // Or annotate that the operation is not supported.
+                op => op.unimplemented().await,
+            }
+        })
+        .await?;
+    }
 
-        Ok(())
-    })
+    Ok(())
 }
 
 async fn getattr<Op>(op: Op) -> Result<Op::Ok, Op::Error>
