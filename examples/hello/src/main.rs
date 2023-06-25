09634dc6b2dba1c8b4733393a3400b7ffb553723
Adeodato Simó <dato@users.noreply.github.com>
2023-06-25 18:07:16 -0300

    Fix typo in hello example

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -22,7 +22,7 @@ fn main() -> Result<()> {
     let mut args = pico_args::Arguments::from_env();
 
     let mountpoint: PathBuf = args.free_from_str()?.context("missing mountpoint")?;
-    ensure!(mountpoint.is_dir(), "tmountpoint must be a directory");
+    ensure!(mountpoint.is_dir(), "mountpoint must be a directory");
 
     let session = Session::mount(mountpoint, KernelConfig::default())?;
 
