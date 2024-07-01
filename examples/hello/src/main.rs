59554e2cde17513c7769627428753056620b0be2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-07-01 07:56:28 -0400

    chore: clean up hello example

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,14 +1,11 @@
-#![allow(clippy::unnecessary_mut_passed)]
-#![deny(clippy::unimplemented)]
+use polyfuse::op;
+use polyfuse::reply::{AttrOut, EntryOut, FileAttr, OpenOut, ReaddirOut};
+use polyfuse::{KernelConfig, Operation, Request, Session};
 
-use polyfuse::{
-    op,
-    reply::{AttrOut, EntryOut, FileAttr, ReaddirOut},
-    KernelConfig, Operation, Request, Session,
-};
-
-use anyhow::{ensure, Context as _, Result};
-use std::{io, os::unix::prelude::*, path::PathBuf, time::Duration};
+use std::io;
+use std::os::unix::prelude::*;
+use std::path::PathBuf;
+use std::time::Duration;
 
 const TTL: Duration = Duration::from_secs(60 * 60 * 24 * 365);
 const ROOT_INO: u64 = 1;
@@ -16,16 +13,27 @@ const HELLO_INO: u64 = 2;
 const HELLO_FILENAME: &str = "hello.txt";
 const HELLO_CONTENT: &[u8] = b"Hello, world!\n";
 
-fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     let mut args = pico_args::Arguments::from_env();
 
-    let mountpoint: PathBuf = args.free_from_str()?.context("missing mountpoint")?;
-    ensure!(mountpoint.is_dir(), "tmountpoint must be a directory");
+    let mountpoint: PathBuf = match args.free_from_str() {
+        Ok(mp) => mp,
+        Err(_) => {
+            tracing::error!("missing mountpoint");
+            std::process::exit(1);
+        }
+    };
+
+    if !mountpoint.is_dir() {
+        tracing::error!("mountpoint must be a directory");
+        std::process::exit(1);
+    }
 
     let session = Session::mount(mountpoint, KernelConfig::default())?;
-
     let fs = Hello::new();
 
     while let Some(req) = session.next_request()? {
@@ -34,17 +42,15 @@ fn main() -> Result<()> {
             Operation::Getattr(op) => fs.getattr(&req, op)?,
             Operation::Read(op) => fs.read(&req, op)?,
             Operation::Readdir(op) => fs.readdir(&req, op)?,
-            _ => req.reply_error(libc::ENOSYS)?,
+            Operation::Opendir(op) => fs.opendir(&req, op)?,
+            unk_op => {
+                tracing::warn!(?unk_op, "unimplemented operation");
+                req.reply_error(libc::ENOSYS)?;
+            }
         }
     }
 
     Ok(())
-}
-
-struct Hello {
-    entries: Vec<DirEntry>,
-    uid: u32,
-    gid: u32,
 }
 
 struct DirEntry {
@@ -53,19 +59,83 @@ struct DirEntry {
     typ: u32,
 }
 
+struct Hello {
+    entries: Vec<DirEntry>,
+    uid: u32,
+    gid: u32,
+}
+
 impl Hello {
+    fn dir_entries(&self) -> impl Iterator<Item = (u64, &DirEntry)> + '_ {
+        self.entries.iter().enumerate().map(|(i, ent)| {
+            let offset = (i + 1) as u64;
+            (offset, ent)
+        })
+    }
+
+    fn fill_hello_attr(&self, attr: &mut FileAttr) {
+        attr.ino(HELLO_INO);
+        attr.size(HELLO_CONTENT.len() as u64);
+        attr.mode(libc::S_IFREG | 0o444);
+        attr.nlink(1);
+        attr.uid(self.uid);
+        attr.gid(self.gid);
+    }
+
+    fn fill_root_attr(&self, attr: &mut FileAttr) {
+        attr.ino(ROOT_INO);
+        attr.mode(libc::S_IFDIR | 0o555);
+        attr.nlink(2);
+        attr.uid(self.uid);
+        attr.gid(self.gid);
+    }
+
+    fn getattr(&self, req: &Request, op: op::Getattr<'_>) -> io::Result<()> {
+        let fill_attr = match op.ino() {
+            ROOT_INO => Self::fill_root_attr,
+            HELLO_INO => Self::fill_hello_attr,
+            _ => return req.reply_error(libc::ENOENT),
+        };
+
+        let mut out = AttrOut::default();
+        fill_attr(self, out.attr());
+        out.ttl(TTL);
+
+        req.reply(out)
+    }
+
+    fn lookup(&self, req: &Request, op: op::Lookup<'_>) -> io::Result<()> {
+        match op.parent() {
+            ROOT_INO if op.name().as_bytes() == HELLO_FILENAME.as_bytes() => {
+                let mut out = EntryOut::default();
+
+                self.fill_hello_attr(out.attr());
+
+                out.ino(HELLO_INO);
+                out.ttl_attr(TTL);
+                out.ttl_entry(TTL);
+
+                req.reply(out)
+            }
+            _ => req.reply_error(libc::ENOENT),
+        }
+    }
+
     fn new() -> Self {
         let mut entries = Vec::with_capacity(3);
+
         entries.push(DirEntry {
             name: ".",
             ino: ROOT_INO,
             typ: libc::DT_DIR as u32,
         });
+
         entries.push(DirEntry {
             name: "..",
             ino: ROOT_INO,
             typ: libc::DT_DIR as u32,
         });
+
         entries.push(DirEntry {
             name: HELLO_FILENAME,
             ino: HELLO_INO,
@@ -79,47 +149,13 @@ impl Hello {
         }
     }
 
-    fn fill_root_attr(&self, attr: &mut FileAttr) {
-        attr.ino(ROOT_INO);
-        attr.mode(libc::S_IFDIR | 0o555);
-        attr.nlink(2);
-        attr.uid(self.uid);
-        attr.gid(self.gid);
-    }
-
-    fn fill_hello_attr(&self, attr: &mut FileAttr) {
-        attr.ino(HELLO_INO);
-        attr.size(HELLO_CONTENT.len() as u64);
-        attr.mode(libc::S_IFREG | 0o444);
-        attr.nlink(1);
-        attr.uid(self.uid);
-        attr.gid(self.gid);
-    }
-
-    fn lookup(&self, req: &Request, op: op::Lookup<'_>) -> io::Result<()> {
-        match op.parent() {
-            ROOT_INO if op.name().as_bytes() == HELLO_FILENAME.as_bytes() => {
-                let mut out = EntryOut::default();
-                self.fill_hello_attr(out.attr());
-                out.ino(HELLO_INO);
-                out.ttl_attr(TTL);
-                out.ttl_entry(TTL);
-                req.reply(out)
-            }
-            _ => req.reply_error(libc::ENOENT),
+    fn opendir(&self, req: &Request, op: op::Opendir<'_>) -> io::Result<()> {
+        if op.ino() != ROOT_INO {
+            return req.reply_error(libc::ENOTDIR);
         }
-    }
 
-    fn getattr(&self, req: &Request, op: op::Getattr<'_>) -> io::Result<()> {
-        let fill_attr = match op.ino() {
-            ROOT_INO => Self::fill_root_attr,
-            HELLO_INO => Self::fill_hello_attr,
-            _ => return req.reply_error(libc::ENOENT),
-        };
-
-        let mut out = AttrOut::default();
-        fill_attr(self, out.attr());
-        out.ttl(TTL);
+        let mut out = OpenOut::default();
+        out.fh(op.ino());
 
         req.reply(out)
     }
@@ -141,13 +177,6 @@ impl Hello {
         }
 
         req.reply(data)
-    }
-
-    fn dir_entries(&self) -> impl Iterator<Item = (u64, &DirEntry)> + '_ {
-        self.entries.iter().enumerate().map(|(i, ent)| {
-            let offset = (i + 1) as u64;
-            (offset, ent)
-        })
     }
 
     fn readdir(&self, req: &Request, op: op::Readdir<'_>) -> io::Result<()> {
