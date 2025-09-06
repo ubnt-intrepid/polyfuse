57285be34c992ed1294fd0db3eee0462f6ae3b89
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2025-09-06 09:27:49 +0000

    deploy: f01e2b8224ae9cb35ceabc65b6a02bb74ba94015

---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["polyfuse",["",[["bytes",[],["decoder.rs"]]],["bytes.rs","conn.rs","fs.rs","lib.rs","mount.rs","nix.rs","notify.rs","op.rs","reply.rs","request.rs","session.rs","types.rs"]]],\
+["polyfuse_kernel",["",[],["lib.rs"]]]\
+]'));
+createSrcSidebar();
